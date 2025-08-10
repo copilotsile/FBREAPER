e740681da39b020b178c0f8516e3b907df890420
copilotsile <copiltosileshi@proton.me>
2025-08-10 18:45:17 +0300

    Delete backend-java directory

--- backend-java/src/main/java/com/fbreaperv1/exception/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.fbreaperv1.exception;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(String message) {
-        super(message);
-    }
-}
--- backend-java/src/main/java/com/fbreaperv1/exception/ScraperException.java
+++
@@ -1,7 +0,0 @@
-package com.fbreaperv1.exception;
-
-public class ScraperException extends RuntimeException {
-    public ScraperException(String message) {
-        super(message);
-    }
-}
